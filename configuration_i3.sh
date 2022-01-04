8e3efcf2a6388cdca4a01e7cc19cbf4599124acd
gwMuraros <gw.muraro@gmail.com>
2022-01-04 22:35:37 +0100

    fixing '\r' shennanigans + escape chars

--- configuration_i3.sh
+++ configuration_i3.sh
@@ -2,18 +2,10 @@ cd ~/.config/i3/
 
 echo "## Self made config " >> config
 echo "" >> config
-echo "set $alt Mod1 " >> config
-echo "" >> config
-echo "# brightness with light" >> config
-echo "bindsym XF86MonBrightnessUp exec light -A 5" >> config
-echo "bindsym XF86MonBrightnessDown exec light -U 5" >> config
+echo "set \$alt Mod1 " >> config
 echo "" >> config
 echo "# alt tab" >> config
-echo "exec --no-startup-id alttab -fg "#d58681" -bg "#4a4a4a" -frame "#eb564d" -t 128x150 -i 127x64 -d 1" >> config
-echo "" >> config
-echo "# Separate workspace " >> config
-echo "assign[class="Firefox"] 2" >> config
-echo "assign[class="Code"] 3" >> config
+echo "exec --no-startup-id alttab -fg \"\#d58681\" -bg \"\#4a4a4a\" -frame \"\#eb564d\" -t 128x150 -i 127x64 -d 1" >> config
 echo "" >> config
 echo "# Launch all what you need " >> config
 echo "exec --no-startup-id i3-sensible-terminal" >> config
