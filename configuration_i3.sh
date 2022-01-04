d2968dda85cd21dc59aa17e7698dbc7c80ae8766
gwMuraros <gw.muraro@gmail.com>
2022-01-04 22:46:03 +0100

    fixing escape chars

--- configuration_i3.sh
+++ configuration_i3.sh
@@ -5,7 +5,7 @@ echo "" >> config
 echo "set \$alt Mod1 " >> config
 echo "" >> config
 echo "# alt tab" >> config
-echo "exec --no-startup-id alttab -fg \"\#d58681\" -bg \"\#4a4a4a\" -frame \"\#eb564d\" -t 128x150 -i 127x64 -d 1" >> config
+echo "exec --no-startup-id alttab -fg \"#d58681\" -bg \"#4a4a4a\" -frame \"#eb564d\" -t 128x150 -i 127x64 -d 1" >> config
 echo "" >> config
 echo "# Launch all what you need " >> config
 echo "exec --no-startup-id i3-sensible-terminal" >> config
